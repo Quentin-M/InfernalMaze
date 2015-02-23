ab413fb90eb160354f83ee73ea462a2186f7db45
Quentin MACHU <hello@quentin-machu.fr>
2015-02-23 15:54:09 +0100

    Added the game window with fixed-rate update/render functions

---
+++ src/fr/quentinmachu/infernalmaze/game/Game.java
@@ -0,0 +1,16 @@
+package fr.quentinmachu.infernalmaze.game;
+
+import fr.quentinmachu.infernalmaze.ui.GameWindow;
+
+public class Game extends GameWindow {
+
+	@Override
+	public void update() {
+
+	}
+
+	@Override
+	public void render(float alpha) {
+
+	}
+}
---
+++ src/fr/quentinmachu/infernalmaze/ui/State.java
@@ -0,0 +1,6 @@
+package fr.quentinmachu.infernalmaze.ui;
+
+public interface State {
+	public void update();
+	public void render(float alpha);
+}
