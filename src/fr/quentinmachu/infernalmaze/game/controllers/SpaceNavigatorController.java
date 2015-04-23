22d70d29c52eac9ad716c36e31e261c404be7697
Quentin MACHU <hello@quentin-machu.fr>
2015-04-23 17:10:38 +0200

    Added support for SpaceNavigator on Windows

--- src/fr/quentinmachu/infernalmaze/game/controllers/SpaceNavigatorController.java
+++ src/fr/quentinmachu/infernalmaze/game/controllers/SpaceNavigatorController.java
@@ -10,73 +10,97 @@ import net.java.games.input.ControllerEnvironment;
 import fr.quentinmachu.infernalmaze.game.Game;
 
 public class SpaceNavigatorController extends InputController {
-	// Internal variables
-	private Controller spaceNavigator;
-	private Component[] components;
-	
-	public SpaceNavigatorController(Game game) {
-		super(game);
-	}
-	
-	@Override
-	public void init() {
-    	// Hide mouse
-    	glfwSetInputMode(game.getWindow(), GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
-    	
-		ControllerEnvironment controllerEnvironment = ControllerEnvironment.getDefaultEnvironment();
-        Controller[] controllers = controllerEnvironment.getControllers();
-        for(Controller controller: controllers){
-            if ("SpaceNavigator".equalsIgnoreCase(controller.getName())){
-                spaceNavigator = controller;
-                components = spaceNavigator.getComponents();
-            }
-        }
-		
-        if(spaceNavigator == null) {
-        	throw new RuntimeException("Could not find any Space Navigator controller.");
-        }
-        
-		// Disable mouse
-		glfwSetInputMode(game.getWindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    // Internal variables
+    private Controller spaceNavigator;
+    private Component[] components;
+
+    public SpaceNavigatorController(Game game) {
+	super(game);
+    }
+
+    @Override
+    public void init() {
+	// Hide mouse
+	glfwSetInputMode(game.getWindow(), GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
+	ControllerEnvironment controllerEnvironment = ControllerEnvironment.getDefaultEnvironment();
+	Controller[] controllers = controllerEnvironment.getControllers();
+	for (Controller controller : controllers) {
+	    if ("SpaceNavigator".equalsIgnoreCase(controller.getName())) {
+		spaceNavigator = controller;
+		components = spaceNavigator.getComponents();
+	    }
 	}
 
-	@Override
-	public void poll() {
-		if (spaceNavigator.poll()) {
-            for(Component component: components) {
-                switch(component.getName()) {
-                    case "x":
-                    	setX((float) (component.getPollData()*MAX_MOVEMENT/0.26));
-                        break;
-                    case "y":
-                    	setY((float) (component.getPollData()*MAX_MOVEMENT/0.26));
-                        break;
-                    case "z":
-                    	setZ((float) (component.getPollData()*MAX_MOVEMENT/0.26));
-                        break;
-                    case "rx":
-                    	setRx((float) (component.getPollData()*MAX_ROTATION_XY/0.26));
-                        break;
-                    case "ry":
-                    	setRy((float) (component.getPollData()*MAX_ROTATION_XY/0.26));
-                        break;
-                    case "rz":
-                        setRz((float) (component.getPollData()*MAX_ROTATION_Z/0.26));
-                        break;
-                    case "0":
-                    	if(component.getPollData()==1.0) setButton0(true);
-                    	else setButton0(false);
-                    	break;
-                    case "1":
-                    	if(component.getPollData()==1.0) setButton1(true);
-                    	else setButton1(false);
-                }
-            }
-        }
+	if (spaceNavigator == null) {
+	    throw new RuntimeException("Could not find any Space Navigator controller.");
 	}
-	
-	@Override
-	public void dispose() {
-		
+
+	// Disable mouse
+	glfwSetInputMode(game.getWindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    }
+
+    @Override
+    public void poll() {
+	if (spaceNavigator.poll()) {
+	    for (Component component : components) {
+		switch (component.getName()) {
+
+		case "Axe X":
+		case "x":
+		    setX((float) (component.getPollData() * MAX_MOVEMENT / 0.26));
+		    break;
+
+		case "Axe Y":
+		case "y":
+		    setY((float) (component.getPollData() * MAX_MOVEMENT / 0.26));
+		    break;
+
+		case "Axe Z":
+		case "z":
+		    setZ((float) (component.getPollData() * MAX_MOVEMENT / 0.26));
+		    break;
+
+		case "Rotation X":
+		case "rx":
+		    setRx((float) (component.getPollData() * MAX_ROTATION_XY / 0.26));
+		    break;
+
+		case "Rotation Y":
+		case "ry":
+		    setRy((float) (component.getPollData() * MAX_ROTATION_XY / 0.26));
+		    break;
+
+		case "Rotation Z":
+		case "rz":
+		    setRz((float) (component.getPollData() * MAX_ROTATION_Z / 0.26));
+		    break;
+
+		case "Bouton 0":
+		case "0":
+		    if (component.getPollData() == 1.0)
+			setButton0(true);
+		    else
+			setButton0(false);
+		    break;
+
+		case "Bouton 1":
+		case "1":
+		    if (component.getPollData() == 1.0)
+			setButton1(true);
+		    else
+			setButton1(false);
+		    break;
+
+		default:
+		    System.out.println("Axe non supporté (pilote?): " + component.getName());
+		}
+	    }
 	}
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
